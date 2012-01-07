a5668e75884453268c0ce753320be2adfc5d746e
Sarah Kreidler <sarah5280@gmail.com>
2012-01-07 21:42:04 +0000

    cleanup/comments

--- src/edu/ucdenver/bios/glimmpseweb/client/wizard/WizardStepPanel.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/wizard/WizardStepPanel.java
@@ -1,30 +1,65 @@
+/*
+ * Web Interface for the GLIMMPSE Software System.  Allows
+ * users to perform power, sample size, and detectable difference
+ * calculations. 
+ * 
+ * Copyright (C) 2010 Regents of the University of Colorado.  
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package edu.ucdenver.bios.glimmpseweb.client.wizard;
 
 import com.google.gwt.user.client.ui.Composite;
 
-//import edu.cudenver.bios.glimmpse.client.listener.StepStatusListener;
-
+/**
+ * Generic superclass for panel representing individual 
+ * "steps" in a wizard
+ * 
+ * @author Sarah Kreidler
+ *
+ */
 public abstract class WizardStepPanel extends Composite
 implements WizardContextListener
 {
+	// context object coordinating user input across screens
 	protected WizardContext context = null;
+	// if true, this panel will be skipped in the wizard navigation
 	protected boolean skip = false;
+	// if true, all required input has been entered for this panel
 	protected boolean complete = false;
-	protected String name = "name";
+	// name of the panel
+	protected String name = "";
 	
-	public WizardStepPanel(String name)
-	{
-		this.name = name;
-	}
-	
-	public WizardStepPanel(String name, WizardContext context)
+	/**
+	 * Create a wizard step panel.  Each step is required for filling
+	 * in a part of the "context" of the wizard.
+	 * 
+	 * @param context context object
+	 * @param name display name of the panel
+	 */
+	public WizardStepPanel(WizardContext context, String name)
 	{
 		this.name = name;
 		this.context = context;
 		this.context.addContextListener(this);
 	}
 	
-	abstract public void reset();
+	/**
+	 * Clear the panel.  Must be implemented in the subclass.
+	 */
+	public abstract void reset();
 	
     /**
      * Notify listeners that this step is complete and forward navigation
@@ -69,6 +104,11 @@ implements WizardContextListener
 	 * Respond to a change in the context object
 	 */
     @Override
-	public void onChange() {}
+	public void onChange(WizardContextChangeEvent e) {}
     
+    /**
+     * Response to a context load event
+     */
+    @Override
+	public void onLoad() {}
 }
