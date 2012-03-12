81bbd7fc15da09a97ac7a8625a1241322b3cb415
Vijay Chander Akula <vijay.akula@ucdenver.edu>
2012-03-12 21:24:58 +0000

    Cancel Dialog Box

--- src/edu/ucdenver/bios/glimmpseweb/client/shared/CancelDialogBox.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/shared/CancelDialogBox.java
@@ -1,3 +1,26 @@
+/*
+ * User Interface for the GLIMMPSE Software System.  Processes
+ * incoming HTTP requests for power, sample size, and detectable
+ * difference
+ * 
+ * Copyright (C) 2011 Regents of the University of Colorado.  
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
 package edu.ucdenver.bios.glimmpseweb.client.shared;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -51,12 +74,6 @@ public Widget cancelDialogBox()
     	
     });
     
-    
-
-    /*saveButton.setWidth("30%");
-    discardChangesButton.setWidth("30%");
-    continueButton.setWidth("30%");*/
-    
     saveButton.setStyleName(STYLE_BUTTON);
     discardChangesButton.setStyleName(STYLE_BUTTON);
     continueButton.setStyleName(STYLE_BUTTON);
@@ -66,16 +83,14 @@ public Widget cancelDialogBox()
     horizontalPanel.add(continueButton);
     horizontalPanel.setSpacing(10);
     
-    
     VerticalPanel verticalPanel = new VerticalPanel();
     verticalPanel.add(query);
     verticalPanel.add(horizontalPanel);
     
-    /*dialogBox.setStyleName(GlimmpseConstants.STYLE_WIZARD_STEP_TABLE_PANEL);*/
     dialogBox.setText("Cancel Dialog Box");
     dialogBox.add(verticalPanel);
     dialogBox.show();
-    /*dialogBox.add(horizontalPanel);*/
+    
 	return dialogBox;
 }
 }
