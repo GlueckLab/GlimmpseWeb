562fe6bb78fe484428a1bce75663e1f88be601e0
Sarah Kreidler <sarah5280@gmail.com>
2012-03-17 22:14:51 +0000

    cleanup of functions related to matrix mode

--- src/edu/ucdenver/bios/glimmpseweb/context/StudyDesignContext.java
+++ src/edu/ucdenver/bios/glimmpseweb/context/StudyDesignContext.java
@@ -10,13 +10,12 @@ import edu.ucdenver.bios.webservice.common.domain.ClusterNode;
 import edu.ucdenver.bios.webservice.common.domain.FixedRandomMatrix;
 import edu.ucdenver.bios.webservice.common.domain.NamedMatrix;
 import edu.ucdenver.bios.webservice.common.domain.StudyDesign;
-import edu.ucdenver.bios.webservice.common.domain.StudyDesignNamedMatrix;
 import edu.ucdenver.bios.webservice.common.enums.SolutionTypeEnum;
 
 public class StudyDesignContext extends WizardContext
 {
 	private StudyDesign studyDesign;
-	
+
 	public StudyDesignContext()
 	{
 		studyDesign = new StudyDesign();
@@ -29,35 +28,35 @@ public class StudyDesignContext extends WizardContext
 
 	public void setAlphaList(WizardStepPanel panel, ArrayList<Double> alphaList)
 	{
-//		studyDesign.setAlphaList(alphaList);
+		//		studyDesign.setAlphaList(alphaList);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.ALPHA_LIST));
 	}
 
 	public void setBetaScaleList(WizardStepPanel panel, List<Double> betaScaleList)
 	{
-//		studyDesign.setAlphaList(betaScaleList);
+		//		studyDesign.setAlphaList(betaScaleList);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.BETA_SCALE_LIST));
 	}
 
 	public void setSigmaScaleList(WizardStepPanel panel, List<Double> sigmaScaleList)
 	{
-//		studyDesign.setAlphaList(sigmaScaleList);
+		//		studyDesign.setAlphaList(sigmaScaleList);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SIGMA_SCALE_LIST));
 	}
 
 	public void setPowerList(WizardStepPanel panel, ArrayList<Double> powerList)
 	{
-//		studyDesign.setPowerList(powerList);
+		//		studyDesign.setPowerList(powerList);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.POWER_LIST));
 	}
 
 	public void setSolutionType(WizardStepPanel panel, SolutionTypeEnum solutionType)
 	{
-//		studyDesign.setSolutionType(solutionType);
+		studyDesign.setSolutionTypeEnum(solutionType);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SOLVING_FOR));
 	}
@@ -69,70 +68,74 @@ public class StudyDesignContext extends WizardContext
 				StudyDesignChangeType.COVARIATE));
 	}
 
-	public void setDesignEssence(WizardStepPanel panel, NamedMatrix designEssence)
+	public void setDesignEssenceMatrix(WizardStepPanel panel, NamedMatrix designFixed,
+			NamedMatrix designRandom)
 	{
-//		studyDesign.setDesignEssence(designEssence);
+		studyDesign.setNamedMatrix(designFixed);
+		if (designRandom != null) studyDesign.setNamedMatrix(designRandom);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.DESIGN_ESSENCE_MATRIX));
 	}
 
 	public void setBetweenParticipantContrast(WizardStepPanel panel, 
-			FixedRandomMatrix betweenParticipantContrast)
+			NamedMatrix fixed, NamedMatrix random)
 	{
-//		studyDesign.setBetweenParticipantContrast(betweenParticipantContrast);
+		studyDesign.setNamedMatrix(fixed);
+		if (random != null) studyDesign.setNamedMatrix(random);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.BETWEEN_CONTRAST_MATRIX));
 	}
 
 	public void setWithinParticipantContrast(WizardStepPanel panel, NamedMatrix withinParticipantContrast)
 	{
-//		studyDesign.setWithinParticipantContrast(withinParticipantContrast);
+		studyDesign.setNamedMatrix(withinParticipantContrast);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.WITHIN_CONTRAST_MATRIX));
 	}
 
-	public void setBeta(WizardStepPanel panel, FixedRandomMatrix beta)
+	public void setBeta(WizardStepPanel panel, NamedMatrix fixed, NamedMatrix random)
 	{
-//		studyDesign.setBeta(beta);
+		studyDesign.setNamedMatrix(fixed);
+		if (random != null) studyDesign.setNamedMatrix(random);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.BETA_MATRIX));
 	}
 
 	public void setSigmaCovariate(WizardStepPanel panel, NamedMatrix sigmaCovariate)
 	{
-//		studyDesign.setSigmaCovariate(sigmaCovariate);
+		//		studyDesign.setSigmaCovariate(sigmaCovariate);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SIGMA_COVARIATE_MATRIX));
 	}
 
 	public void setSigmaError(WizardStepPanel panel, NamedMatrix sigmaError)
 	{
-//		studyDesign.setSigmaError(sigmaError);
+		//		studyDesign.setSigmaError(sigmaError);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SIGMA_ERROR_MATRIX));
 	}
 
 	public void setSigmaOutcomesCovariate(WizardStepPanel panel, NamedMatrix sigmaYG)
 	{
-//		studyDesign.setSigmaOutcomesCovariate(sigmaYG);
+		//		studyDesign.setSigmaOutcomesCovariate(sigmaYG);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SIGMA_OUTCOME_COVARIATE_MATRIX));
 	}
 
 	public void setSigmaOutcomes(WizardStepPanel panel, NamedMatrix sigmaY)
 	{
-//		studyDesign.setSigmaOutcomes(sigmaY);
+		//		studyDesign.setSigmaOutcomes(sigmaY);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.SIGMA_OUTCOME_MATRIX));
 	}
 
 	public void setThetaNull(WizardStepPanel panel, NamedMatrix thetaNull)
 	{
-//		studyDesign.setThetaNull(thetaNull);
+		//		studyDesign.setThetaNull(thetaNull);
 		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
 				StudyDesignChangeType.THETA_NULL_MATRIX));
 	}
-	
+
 	/**
 	 * Store clustering information to the local context and the study design service, and
 	 * notify all wizard panels of the change.
@@ -142,8 +145,8 @@ public class StudyDesignContext extends WizardContext
 	 */
 	public void setClustering(WizardStepPanel panel, ArrayList<ClusterNode> clusteringNodeList)
 	{
-	    studyDesign.setClusteringTree(clusteringNodeList);
-	    notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
-	            StudyDesignChangeType.CLUSTERING));
+		studyDesign.setClusteringTree(clusteringNodeList);
+		notifyWizardContextChanged(new StudyDesignChangeEvent(panel, 
+				StudyDesignChangeType.CLUSTERING));
 	}
 }
