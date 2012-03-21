9cffae3f9e7cce297f6f5351626498e00b94c6f2
Sarah Kreidler <sarah5280@gmail.com>
2012-03-21 01:42:50 +0000

    now loads Google Visualization API

--- src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
@@ -3,9 +3,9 @@ package edu.ucdenver.bios.glimmpseweb.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.visualizations.Table;
 
-import edu.ucdenver.bios.glimmpseweb.client.guided.GuidedWizardPanel;
-import edu.ucdenver.bios.glimmpseweb.client.matrix.MatrixWizardPanel;
 import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseApplicationPanel;
 
 /**
@@ -19,27 +19,34 @@ public class GlimmpseWeb implements EntryPoint
 	// string constants for internationalization 
 	public static final GlimmpseConstants constants =  
 		(GlimmpseConstants) GWT.create(GlimmpseConstants.class); 
+	
+	/**
+	 * Load the GLIMMPSE application
+	 */
+    Runnable onLoadCallback = new Runnable() {
+        public void run() {         
+            // add the main application
+            RootPanel glimmpseApp = RootPanel.get("glimmpseApp");
+            if (glimmpseApp != null)
+            {
+                glimmpseApp.add(new GlimmpseApplicationPanel());
+                glimmpseApp.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
+            }
 
+            // set root style so it recognizes standard css elements like "body"
+            RootPanel.get().setStyleName("body");
+        }
+    };
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad()
 	{        
-		// add the main application
-		RootPanel glimmpseApp = RootPanel.get("glimmpseApp");
-		if (glimmpseApp != null)
-		{
-			glimmpseApp.add(new GlimmpseApplicationPanel());
-			glimmpseApp.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
-		}
-
-		// set root style so it recognizes standard css elements like "body"
-		RootPanel.get().setStyleName("body");
-
 		// Load the visualization api, passing the onLoadCallback to be called
 		// when loading is done.
-//		VisualizationUtils.loadVisualizationApi(onLoadCallback, 
-//				Table.PACKAGE);
+		VisualizationUtils.loadVisualizationApi(onLoadCallback, 
+				Table.PACKAGE);
 	}
 }
 
