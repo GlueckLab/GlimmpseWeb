86470eff0cbe7356bbad8c4c28cc854fefa6e2ab
Sarah Kreidler <sarah5280@gmail.com>
2012-08-18 03:57:22 +0000

    added a feedback form for bug submission

--- src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
@@ -1,3 +1,24 @@
+/*
+ * User Interface for the GLIMMPSE Software System.  Allows
+ * users to perform power, sample size, and detectable difference
+ * calculations. 
+ * 
+ * Copyright (C) 2010 Regents of the University of Colorado.  
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package edu.ucdenver.bios.glimmpseweb.client;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -7,6 +28,7 @@ import com.google.gwt.visualization.client.VisualizationUtils;
 import com.google.gwt.visualization.client.visualizations.Table;
 
 import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseApplicationPanel;
+import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseFeedbackPanel;
 
 /**
  * Entry point class for the GLIMMPSE web interface.
@@ -31,6 +53,14 @@ public class GlimmpseWeb implements EntryPoint
             {
                 glimmpseApp.add(new GlimmpseApplicationPanel());
                 glimmpseApp.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
+            }
+            
+            // add the feedback panel
+            RootPanel glimmpseFeedback = RootPanel.get("glimmpseFeedback");
+            if (glimmpseFeedback != null)
+            {
+                glimmpseFeedback.add(new GlimmpseFeedbackPanel());
+                glimmpseFeedback.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
             }
 
             // set root style so it recognizes standard css elements like "body"
