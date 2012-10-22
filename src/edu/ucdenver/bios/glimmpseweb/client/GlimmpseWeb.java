02f900fa345067262f2da51370d40d66ed7adc85
Sarah Kreidler <sarah5280@gmail.com>
2012-10-22 16:25:24 +0000

    merge from trunk

--- src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/GlimmpseWeb.java
@@ -23,9 +23,8 @@ package edu.ucdenver.bios.glimmpseweb.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.visualization.client.VisualizationUtils;
-import com.google.gwt.visualization.client.visualizations.Table;
 
 import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseApplicationPanel;
 import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseFeedbackPanel;
@@ -35,48 +34,42 @@ import edu.ucdenver.bios.glimmpseweb.client.shared.GlimmpseFeedbackPanel;
  */
 public class GlimmpseWeb implements EntryPoint
 {
-	/**
-	 * This is the entry point method.
-	 */
-	// string constants for internationalization 
-	public static final GlimmpseConstants constants =  
-		(GlimmpseConstants) GWT.create(GlimmpseConstants.class); 
-	
-	/**
-	 * Load the GLIMMPSE application
-	 */
-    Runnable onLoadCallback = new Runnable() {
-        public void run() {         
-            // add the main application
-            RootPanel glimmpseApp = RootPanel.get("glimmpseApp");
-            if (glimmpseApp != null)
-            {
-                glimmpseApp.add(new GlimmpseApplicationPanel());
-                glimmpseApp.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
-            }
-            
-            // add the feedback panel
-            RootPanel glimmpseFeedback = RootPanel.get("glimmpseFeedback");
-            if (glimmpseFeedback != null)
-            {
-                glimmpseFeedback.add(new GlimmpseFeedbackPanel());
-                glimmpseFeedback.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
-            }
-
-            // set root style so it recognizes standard css elements like "body"
-            RootPanel.get().setStyleName("body");
-        }
-    };
+    /**
+     * This is the entry point method.
+     */
+    // string constants for internationalization 
+    public static final GlimmpseConstants constants =  
+        (GlimmpseConstants) GWT.create(GlimmpseConstants.class); 
 	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad()
 	{        
-		// Load the visualization api, passing the onLoadCallback to be called
-		// when loading is done.
-		VisualizationUtils.loadVisualizationApi(onLoadCallback, 
-				Table.PACKAGE);
+        // remove the splash screen
+        Element loadingDiv =  RootPanel.get("loadingMessage").getElement();
+        if (loadingDiv != null) {
+            loadingDiv.setInnerHTML(" ");
+        }
+	    
+        // add the main application
+        RootPanel glimmpseApp = RootPanel.get("glimmpseApp");
+        if (glimmpseApp != null)
+        {
+            glimmpseApp.add(new GlimmpseApplicationPanel());
+            glimmpseApp.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
+        }
+
+        // add the feedback panel
+        RootPanel glimmpseFeedback = RootPanel.get("glimmpseFeedback");
+        if (glimmpseFeedback != null)
+        {
+            glimmpseFeedback.add(new GlimmpseFeedbackPanel());
+            glimmpseFeedback.setStyleName(GlimmpseConstants.STYLE_GLIMMPSE_PANEL);
+        }
+
+        // set root style so it recognizes standard css elements like "body"
+        RootPanel.get().setStyleName("body");
 	}
 }
 
--- src/edu/ucdenver/bios/glimmpseweb/client/connector/FileSvcConnector.java
+++ src/edu/ucdenver/bios/glimmpseweb/client/connector/FileSvcConnector.java
@@ -1,12 +1,9 @@
 package edu.ucdenver.bios.glimmpseweb.client.connector;
 
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
-import com.google.gwt.visualization.client.DataTable;
 
 import edu.ucdenver.bios.glimmpseweb.client.GlimmpseWeb;
 import edu.ucdenver.bios.webservice.common.domain.StudyDesign;
@@ -79,10 +76,10 @@ public class FileSvcConnector extends Composite {
      * @param data the data to be saved
      * @param filename default filename to use
      */
-    public void saveDataTableAsCSV(DataTable dataTable, String filename)
+    public void saveStringToFile(String str, String filename)
     {
-        if (dataTable != null) {
-            dataHidden.setValue(dataTableToCSV(dataTable));
+        if (str != null && !str.isEmpty()) {
+            dataHidden.setValue(str);
             String saveFilename = DATA_FILENAME;
             if (filename != null && !filename.isEmpty()) {
                 saveFilename = filename;
@@ -92,40 +89,5 @@ public class FileSvcConnector extends Composite {
         }
         saveForm.reset();
     }
-    
-    /**
-     * Output the data table in CSV format
-     * @return CSV formatted data
-     */
-    private String dataTableToCSV(DataTable dataTable)
-    {
-        StringBuffer buffer = new StringBuffer();
-        
-        if (dataTable.getNumberOfRows() > 0)
-        {
-            // add the column headers
-            for(int col = 0; col < dataTable.getNumberOfColumns(); col++)
-            {
-                if (col > 0) buffer.append(",");
-                buffer.append(dataTable.getColumnId(col));
-            }
-            buffer.append("\n");
-            // now add the data
-            for(int row = 0; row < dataTable.getNumberOfRows(); row++)
-            {
-                for(int col = 0; col < dataTable.getNumberOfColumns(); col++)
-                {
-                    if (col > 0) buffer.append(",");
-                    if (dataTable.getColumnType(col) == ColumnType.STRING)
-                        buffer.append(dataTable.getValueString(row, col));
-                    else    
-                        buffer.append(dataTable.getValueDouble(row, col));    
-                }
-                buffer.append("\n");
-            }
-        }
-        return buffer.toString();
-    }
-    
-    
+     
 }
